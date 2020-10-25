bb57f5500da153196bf308ad3494793a397e4216
Nixolay <i.pro.linux@yandex.ru>
2020-10-25 15:01:42 +0500

    feat: new project robot identifier (#16)
    Co-authored-by: Nikolay <n.kudrin@kontur.ru>

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Nixolay/training
 
 go 1.15
 
-require github.com/smartystreets/goconvey v1.6.4
+require (
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	github.com/pkg/profile v1.5.0
+	github.com/smartystreets/assertions v1.2.0 // indirect
+	github.com/smartystreets/goconvey v1.6.4
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
+)
---
+++ golang/robot_identification/api.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof" //nolint:gci,gosec
+	"time"
+
+	"golang.org/x/net/http2"
+)
+
+var userStorage = CreateUserStorage(time.Minute) //nolint:gochecknoglobals
+
+// RunServer Server API launch.
+func RunServer() {
+	httpServer := http.Server{Addr: ":8080"}
+
+	http2Server := http2.Server{}
+	if err := http2.ConfigureServer(&httpServer, &http2Server); err != nil {
+		log.Fatal(err)
+	}
+
+	createHandlers()
+
+	log.Fatal(httpServer.ListenAndServe())
+}
+
+func createHandlers() {
+	http.HandleFunc("/count", count)
+	http.HandleFunc("/", identification)
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := r.Body.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	fmt.Fprint(w, userStorage.CountRobots())
+}
+
+func identification(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := r.Body.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	if keys, ok := r.URL.Query()["user_id"]; ok && len(keys) > 0 {
+		userStorage.Inc(keys[0])
+	}
+
+	fmt.Fprint(w, "ok")
+}
