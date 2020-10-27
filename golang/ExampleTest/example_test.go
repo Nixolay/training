d08f6679df59c5899440df98559ff1a2fcee0161
Nixolay <i.pro.linux@yandex.ru>
2020-10-27 12:20:54 +0500

    feat: practic example functions (#22)
    Co-authored-by: Nixolay <nixolive@yandex.ru>

---
+++ golang/ExampleTest/example_test.go
@@ -0,0 +1,15 @@
+package ex_test
+
+import (
+	"fmt"
+
+	. "github.com/Nixolay/training/golang/ExampleTest"
+)
+
+func ExampleDouble() {
+	fmt.Println(Double(1))
+	fmt.Println(Double(2))
+	// Output:
+	// 2
+	// 4
+}
