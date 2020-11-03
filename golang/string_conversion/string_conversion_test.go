61edd96a5377e6b55f1b4cc56e5872ce7f35d427
Nixolay <i.pro.linux@yandex.ru>
2020-11-03 16:33:08 +0500

    feat: testing unsafe conversion (#23)
    Co-authored-by: Nikolay <n.kudrin@kontur.ru>

---
+++ golang/string_conversion/string_conversion_test.go
@@ -0,0 +1,88 @@
+package conversion_test
+
+import (
+	"testing"
+
+	. "github.com/Nixolay/training/golang/string_conversion"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const (
+	word = "test data is my big text"
+	size = 1000
+)
+
+func TestStringConversion(t *testing.T) {
+	Convey("Test string conversion", t, func() {
+		Convey("Test bytes conversion to string", func() {
+			data := []byte(word)
+			expected := UnsafeBytesToString(data)
+			So(expected, ShouldEqual, word)
+		})
+
+		Convey("Test bytes conversion to string by Header", func() {
+			data := []byte(word)
+			expected := UnsafeBytesToStringByHeader(data)
+			So(expected, ShouldEqual, word)
+		})
+
+		Convey("Test string conversion to byte slice by Header", func() {
+			actual := []byte(word)
+			expected := UnsafeStringToByteSliceByHeader(word)
+			So(expected, ShouldResemble, actual)
+		})
+	})
+}
+
+func BenchmarkUnsafeBytesToString(b *testing.B) {
+	data := []byte(word)
+	v := make(chan string, size)
+
+	for i := 0; i < size; i++ {
+		v <- UnsafeBytesToString(data)
+	}
+
+	close(v)
+}
+
+func BenchmarkUnsafeBytesToStringByHeader(b *testing.B) {
+	data := []byte(word)
+	v := make(chan string, size)
+
+	for i := 0; i < size; i++ {
+		v <- UnsafeBytesToStringByHeader(data)
+	}
+
+	close(v)
+}
+
+func BenchmarkBytesToString(b *testing.B) {
+	data := []byte(word)
+	v := make(chan string, size)
+
+	for i := 0; i < size; i++ {
+		v <- string(data)
+	}
+
+	close(v)
+}
+
+func BenchmarkUnsafeStringToByteSliceByHeader(b *testing.B) {
+	v := make(chan []byte, size)
+
+	for i := 0; i < size; i++ {
+		v <- UnsafeStringToByteSliceByHeader(word)
+	}
+
+	close(v)
+}
+
+func BenchmarkStringToByteSlice(b *testing.B) {
+	v := make(chan []byte, size)
+
+	for i := 0; i < size; i++ {
+		v <- []byte(word)
+	}
+
+	close(v)
+}
