ac3b2ae380002c41f4b53b88b6fd7c9ac938fb05
n.kudrin <n.kudrin@rt-solar.ru>
2024-04-02 13:04:18 +0300

    FEATURE: cnames
    cnmaes

--- golang/blackHat/dns/geta/main.go
+++ golang/blackHat/dns/geta/main.go
@@ -17,8 +17,21 @@ func main() {
 		panic(err.Error())
 	}
 
+	println(" ---- IPs")
+
 	for _, ip := range ips {
 		println(ip)
+	}
+
+	println(" ---- NAMES")
+
+	names, err := lookupCNAME(*domain, *serv)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, name := range names {
+		println(name)
 	}
 }
 
@@ -46,4 +59,30 @@ func lookupA(fqdn, serverAddr string) ([]string, error) {
 	}
 
 	return ips, nil
+}
+
+func lookupCNAME(fqdn, serverAddr string) ([]string, error) {
+	var (
+		msg   dns.Msg
+		fqdns []string
+	)
+
+	msg.SetQuestion(dns.Fqdn(fqdn), dns.TypeCNAME)
+
+	inMsg, err := dns.Exchange(&msg, serverAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(inMsg.Answer) < 1 {
+		return nil, errors.New("no answer")
+	}
+
+	for _, answer := range inMsg.Answer {
+		if c, ok := answer.(*dns.CNAME); ok {
+			fqdns = append(fqdns, c.Target)
+		}
+	}
+
+	return fqdns, nil
 }
