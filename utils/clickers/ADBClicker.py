256ac4340ddcc1a2892ae44df04affda2bcd0bd3
Nixolay <i.pro.linux@yandex.ru>
2024-07-04 14:27:23 +0300

    FEAT: click meadle
    click meadle screen

--- utils/clickers/ADBClicker.py
+++ utils/clickers/ADBClicker.py
@@ -1,28 +1,41 @@
 #  adb connect 127.0.0.1:62001
-import subprocess
+import subprocess, re
 from random import randint
 from time import sleep
 
 output = subprocess.check_output(["adb", "devices"])
 
+def getScreenSize(addr):
+   output = subprocess.check_output(['adb', "-s", addr, 'shell', 'wm', 'size'], text=True)
+   print(f"Screen: {output}")
+   match = re.search(r"(\d+)\s*x\s*(\d+)", output)
+   if match:
+      return int(match.group(1)), int(match.group(2))
+   else:
+      return 1080, 1920
+
 # Извлечь IP-адреса из строк вывода
-ip_addresses = []
+ip_addresses = {}
 for line in output.decode().split('\n'):
-  if 'device' in line and not 'List of devices attached' in line:
-    ip_addresses.append(line.split()[0])
+   if 'device' in line and not 'List of devices attached' in line:
+      addr = line.split()[0]
+      w, h = getScreenSize(addr)
+      ip_addresses[addr] = [int(w/2), int(h/2)]
 
 print(ip_addresses)
 if len(ip_addresses) == 0:
-   ip_addresses.append('127.0.0.1:62001')
+   addr = '127.0.0.1:62001'
+   w, h = getScreenSize(addr)
+   ip_addresses[addr] = [int(w/2), int(h/2)]
 
 def send():
-    for addr in ip_addresses:
-        x = randint(310, 723)
-        y = randint(912, 1290)
+    for addr, sc in ip_addresses.items():
+        x = randint((sc[0]-200), (sc[0]+200))
+        y = randint(sc[1], (sc[1]+300))
         subprocess.call(["adb", "-s", addr, "shell", "input", "tap", str(x), str(y)], stdout=subprocess.DEVNULL)
 
 while True:
     for _ in range(randint(200, 320)):
         send()
-        sleep(randint(1100, 1200)/1000)
+        sleep(randint(2500, 3000)/1000)
     sleep(randint(20, 40))
